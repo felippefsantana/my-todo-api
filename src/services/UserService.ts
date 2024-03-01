bdb8debb91bc6e9b86f974c1bce8544d01e5d840
Felippe Santana <felippefsantana02@gmail.com>
2024-03-01 18:01:27 -0300

    create user service

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,8 +1,31 @@
-import {
-  createUser,
-  deleteUser,
-  findUserById,
-  updateUser,
-} from "../repositories/UserRepository";
+import User, { IUser } from "../models/User";
+import { hashPassword } from "../utils/hash-password";
 
-export const save = () => {};
+type CreateUserData = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+type UserData = Omit<IUser, "_id">;
+
+export const createUser = async (data: CreateUserData): Promise<IUser> => {
+  const existingUser = await User.findOne({ email: data.email });
+
+  if (existingUser) {
+    throw new Error("Já existe um usuário com este endereço de e-mail!");
+  }
+
+  const passwordHash = await hashPassword(data.password);
+
+  const userData: UserData = {
+    name: data.name,
+    email: data.email,
+    password: passwordHash,
+  };
+
+  const userDoc = new User(userData);
+  const newUser = await userDoc.save();
+
+  return newUser;
+};
