c7a6c930e85c4874f7277dcaafe2558b416c815e
Felippe Santana <felippefsantana02@gmail.com>
2024-03-09 19:27:12 -0300

    refactor validate middleware

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -8,11 +8,24 @@ export const validate =
       await schema.parseAsync(req.body);
       return next();
     } catch (error) {
-      return res
-        .status(400)
-        .json({
-          message: "Dados inválidos",
-          errors: (error as ZodError).errors,
-        });
+      if (error instanceof ZodError) {
+        const response = {
+          error: {
+            code: 400,
+            message: "Dados inválidos",
+            details: error.errors.map((error) => ({
+              field: error.path[0],
+              message: error.message,
+            })),
+          },
+        };
+
+        return res.status(400).json(response);
+
+        // return res.status(400).json({
+        //   message: "Dados inválidos",
+        //   errors: (error as ZodError).errors,
+        // });
+      }
     }
   };
